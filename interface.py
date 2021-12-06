f2157adb9be2dd21f31eb00c94d75489b53e0a3f
Arthur Mathais <mathaiar@ensipc310.ensimag.fr>
2021-12-06 15:31:35 +0100

    interface graphique v4 - select box

--- interface.py
+++ interface.py
@@ -13,18 +13,25 @@ def draw_circle(screen, points, seg, pos):
     screen.fill((255, 255, 255))
     draw_poly(screen, points, seg)
     cercle = find_nearest(points, pos)
-    pygame.draw.circle(screen, color=(255, 0, 0), center=cercle, radius=5)
+    pygame.draw.circle(screen, color=(0, 0, 255), center=cercle, radius=5)
+    return [cercle]
 
 def select(screen, points, seg, pos, pos2):
     liste = inside(points, pos, pos2)
     if len(liste) == 0:
-        draw_circle(screen, points, seg, pos)
+        liste = draw_circle(screen, points, seg, pos)
     else:
         screen.fill((255, 255, 255))
         draw_poly(screen, points, seg)
         for elmt in liste:
             pygame.draw.circle(screen, color=(0, 0, 255), center=elmt, radius=5)
+    return liste
 
+def update(screen, points, seg, liste_cerlces):
+    screen.fill((255, 255, 255))
+    draw_poly(screen, points, seg)
+    for cercle in liste_cerlces:
+        pygame.draw.circle(screen, color=(0, 0, 255), center=cercle, radius=5)
 
 def main_interface(points, seg, WIDTH=1000, HEIGHT=1000):
     pygame.init()
@@ -37,7 +44,11 @@ def main_interface(points, seg, WIDTH=1000, HEIGHT=1000):
     draw_poly(screen, points, seg)
 
     pos = None
-    
+    mousedrag = False
+    mouse_down = False
+    liste_cerlces = []
+
+    update(screen, points, seg, liste_cerlces)
     pygame.display.update()
     while True:
         for event in pygame.event.get():
@@ -46,8 +57,19 @@ def main_interface(points, seg, WIDTH=1000, HEIGHT=1000):
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
-            if event.type == pygame.MOUSEBUTTONUP:
+                mouse_down = True
+            elif event.type == pygame.MOUSEBUTTONUP:
                 pos2 = pygame.mouse.get_pos()
-                select(screen, points, seg, pos, pos2)
-
+                liste_cerlces = select(screen, points, seg, pos, pos2)
+                mouse_down = False
+                mousedrag = False
+            if event.type == pygame.MOUSEMOTION:
+                if mouse_down:
+                    actu_pos = pygame.mouse.get_pos()
+                    mousedrag = True
+        if mousedrag:
+            update(screen, points, seg, liste_cerlces)
+            mouseRectCorners = [pos, [pos[0], actu_pos[1]], actu_pos, [actu_pos[0], pos[1]]]
+            pygame.draw.lines(screen, color=(255, 0, 0), closed=True, points=mouseRectCorners, width=3)
+        
         pygame.display.update()
